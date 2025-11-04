a99c7e859904352414f662e3a8a76d079140adba
ricardojdgferreira <127304581+ricardojdgferreira@users.noreply.github.com>
2025-11-04 09:31:44 +0100

    Switch to BioPython for PDB output handling
    Replaced MDAnalysis PDB output with BioPython to avoid errors with RDKit. Added classes for residue selection using BioPython.

--- package/scoring_function/rt.py
+++ package/scoring_function/rt.py
@@ -1,12 +1,28 @@
 import os
+import parser
+from io import StringIO
 import MDAnalysis as mda
+from MDAnalysis.coordinates.PDB import PDBWriter
 import numpy as np
 import sklearn.metrics
 import oddt
 from oddt import fingerprints
 from oddt.toolkits.rdk import Molecule
+from Bio.PDB import PDBParser, PDBIO, Select
 import time
 
+## ADDED: replacing MDAnalysis PDB output by BioPython (to avoid errors with RDKit) #
+standard_residues = {'ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY','HIS', 'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER','THR', 'TRP', 'TYR', 'VAL'}
+
+# added classes for selections with BioPython
+class ProteinOnly(Select):
+    def accept_residue(self, residue):
+        return residue.get_resname() in standard_residues
+				
+class LigandOnly(Select):
+    def accept_residue(self, residue):
+        return residue.get_resname() == "LIG"
+#####################################################################################
 
 class score:
     def __init__(self, vars):
@@ -16,15 +32,15 @@ class score:
     def reference(self):
         if not os.path.exists('reference_protein.pdb') or not os.path.exists('reference_ligand.pdb'):
 
-            u = mda.Universe(self.solvpdb)
-            protein = u.select_atoms('protein')
-            ligand = u.select_atoms('resname LIG')
-
-            with mda.Writer('reference_protein.pdb', protein.n_atoms) as W:
-                W.write(protein)
-
-            with mda.Writer('reference_ligand.pdb', ligand.n_atoms) as W:
-                W.write(ligand)
+            # moved to BioPython to avoid errors with ODDT/RDKit #
+            parser = PDBParser(QUIET=True)
+            structure = parser.get_structure("complex", self.solvpdb)
+        
+            io = PDBIO()
+            io.set_structure(structure)
+            io.save("reference_protein.pdb", select=ProteinOnly())
+            io.save("reference_ligand.pdb", select=LigandOnly())
+            ######################################################
 
         ref = self.ref_fingerprint('reference_protein.pdb', 'reference_ligand.pdb')
 
@@ -68,14 +84,27 @@ class score:
     def calc_ifp(self, u, i):
         u.trajectory[i]
 
-        u_protein = u.select_atoms('protein')
-        protein_file = f'frame_pdbs/protein_{i}.pdb'
+        # moved to BioPython to avoid errors with ODDT/RDKit #
+        # v0.1 now avoids intermediary files (all on memory)
+        protein_file = f'frame_pdbs/protein_{i}.pdb'       
+        
+        pdb_buffer = StringIO()
+        writer = PDBWriter(pdb_buffer)
+        writer.write(u.atoms)
+        pdb_buffer.seek(0)
+        
+        parser = PDBParser(QUIET=True)
+        structure = parser.get_structure(f"protein_{i}", pdb_buffer)
+        writer.close()
+        
+        io = PDBIO()
+        io.set_structure(structure)
+        io.save(protein_file, select=ProteinOnly())
+        #####################################################
 
+        # original code from here onwards
         u_ligand = u.select_atoms('resname LIG')
         ligand_file = f'frame_pdbs/ligand_{i}.pdb'
-
-        with mda.Writer(protein_file, u_protein.n_atoms) as w:
-            w.write(u_protein)
 
         with mda.Writer(ligand_file, u_ligand.n_atoms) as w:
             w.write(u_ligand)
@@ -94,9 +123,9 @@ class score:
         matrix = np.stack(l_plif_temp, axis=0)
         idx = np.argwhere(np.all(matrix[..., :] == 0, axis=0))
         matrix_dense = np.delete(matrix, idx, axis=1)
-        x=matrix_dense[0].reshape(1,-1)
-        y=matrix_dense[1].reshape(1,-1)
-        sim_giovanni=float(sklearn.metrics.pairwise.cosine_similarity(x, y))
+        x = matrix_dense[0].reshape(1,-1)
+        y = matrix_dense[1].reshape(1,-1)
+        sim_giovanni = float(sklearn.metrics.pairwise.cosine_similarity(x, y))
         sim = round(sim_giovanni * -1,2)
 
         os.system(f'rm -r {protein_file} {ligand_file}')
